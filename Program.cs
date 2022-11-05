df61a76a70a181f4b422f9664e730c8b76b906d7
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-04 22:22:42 -0300

    [04/11/2022] Sequência de Limite

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Sequência de Limite.");
+Console.Write("Escolha um número para iniciar: ");
+int begin = Convert.ToInt32(Console.ReadLine());
+Console.Write("Escolha um número para finalizar: ");
+int end = Convert.ToInt32(Console.ReadLine());
+if (begin <= end)
+{
+while (begin <= end)
+{
+Console.Write($"{begin} ");
+begin = begin + 1;
+}
+Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("O numero inicial é menor que o final, tente novamente.");
+}
+Console.WriteLine();
